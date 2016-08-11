0b009e290490d8f6cb9463b3aa97f4a14cb2ad6f
Julien <julien.rougeron@gmail.com>
2016-08-11 17:40:58 -0600

    add content script to show popup to authenticate !

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,26 +1,55 @@
-var url = "http://bk-it.herokuapp.com/api/";
+var api_url = "http://bk-it.herokuapp.com/api/";
 
-chrome.browserAction.onClicked.addListener(function (tab) { //Fired when User Clicks ICON
-	console.log('tab.url', tab.url);
+function addBookmark (tab, token) {
 	$.ajax({
-        url: url + "bookmarks",
+        url: api_url + "bookmarks",
         type: "post",
         dataType: "json",
-        data:JSON.stringify({
-        	"user": 1,
-		    "title": "",
-		    "categories": [],
-		    "favorite": false,
-		    "source": "",
-		    "tosort": false,
-		    "toread": false,
-		    "url": tab.url
-        }),
         contentType: "application/json; charset=utf-8",
-        success: function (result) {
+        data: JSON.stringify({
+  		    "title": tab.title,
+  		    "url": tab.url,
+          "categories": [],
+          "keywords": []
+        }),
+        headers: {
+          "Authorization": "Token " + token
         },
-        error: function() {
-            $('#res').html('An error occurred');
+        success: function (result) {
+          console.log('result', result);
+        },
+        error: function(error) {
+          console.log('error', error);
         }
-	}); 	
-});
+	});
+};
+
+//example of using a message handler from the inject scripts
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+  	if (request['bk-it_token']) {
+			chrome.storage.sync.set({
+				'bk-it_token': request['bk-it_token']
+			}, function() {
+        addBookmark(sender.tab, request['bk-it_token']);
+      });
+		}
+  });
+
+	chrome.browserAction.onClicked.addListener(function(tab) {
+		chrome.storage.sync.get('bk-it_token', function(token) {
+			if (token && token['bk-it_token']) {
+				// save bookmark
+				//on success
+				chrome.tabs.executeScript(null, { file: "js/jquery/jquery.min.js" }, function() {
+					chrome.tabs.insertCSS(null, { file: "src/inject/save_bookmark.css" });
+				  chrome.tabs.executeScript(null, { file: "src/inject/save_bookmark.js" });
+				});
+			} else {
+				chrome.tabs.executeScript(null, { file: "js/jquery/jquery.min.js" }, function() {
+					chrome.tabs.insertCSS(null, { file: "src/inject/login.css" });
+					chrome.tabs.executeScript(null, { file: "src/inject/login.js" });
+				});
+			}
+		});
+	});
---
+++ src/inject/login.js
@@ -0,0 +1,25 @@
+var login_url = "//bk-it.herokuapp.com/auth/login/",
+    template = '<div id="bkit">' +
+      '<form class="form">' +
+        'Username: <input type="text" name="username"><br>' +
+        'Password: <input type="password" name="password"><br>' +
+        '<input type="submit" value="Submit">' +
+      '</form>' +
+    '</div>';
+
+$('body').append(template);
+
+$('#bkit form').submit(function(e){
+  e.preventDefault();
+  var data = {
+    username: $(this).find('[name=username]').val(),
+    password: $(this).find('[name=password]').val(),
+  }
+  console.log('data bef', data );
+  $.post(login_url, data, function( response ) {
+    console.log(response);
+    chrome.runtime.sendMessage({
+      'bk-it_token': response.auth_token
+    });
+  });
+})
